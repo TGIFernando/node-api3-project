f66b45df636bacf6518ac1d82aa775c81c028449
Fernando Chavez <fjchavez01@gmail.com>
2020-12-02 12:45:16 -0800

    finished logger and got server up and running with GET/api/users

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const userRouter = require('./users/userRouter')
+const cors = require('cors')
 const server = express();
+
+server.use(cors())
+server.use(logger)
+
+server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +14,10 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const date = new Date()
+  console.log(req.method, req.url, date, '\n')
+  next()
+}
 
-module.exports = server;
+module.exports = server
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const db = require('./userDb')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // do your magic!
 });
 
@@ -10,8 +11,19 @@ router.post('/:id/posts', (req, res) => {
   // do your magic!
 });
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // do your magic!
+  try{
+    const users = await db.get()
+    if (!users) {
+      res.status(404).json({ message: 'resource not found'})
+    } else {
+      res.status(200).json(users)
+    }
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({message: 'oops looks like something went wrong with the server'})
+  }
 });
 
 router.get('/:id', (req, res) => {
